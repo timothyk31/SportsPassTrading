ab8f23378cd1433f7bc385ff84a5f9d01ccbd019
ryantran <113406848+rryantran@users.noreply.github.com>
2024-08-19 10:14:54 -0500

    update dependencies + minor edits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './components/card'
-import Navbar from './components/navbar'
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar />
-      <div className='grid'>
+      <div className="grid">
         <Card contact="111-222-3333" />
         <Card contact="111-222-3333" />
         <Card contact="111-222-3333" />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
